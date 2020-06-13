be3788edbc12e8e428fbf5d810a7263088c66ee6
kacystocks <kstocks0811@gmail.com>
2020-06-12 19:25:21 -0600

    Cleaned up and commented

--- myRGB/myRGB.ino
+++ myRGB/myRGB.ino
@@ -1,5 +1,6 @@
-
-
+// Lesson 4 RGB Led + Fade in and out
+// This is an Arduino Mega 2560 project
+// made for fun by Kacy Stocks- June 12th, 2020
 #include <stdlib.h>
 #include <time.h>
 
@@ -7,12 +8,8 @@
 #define BLUE 3
 #define GREEN 5
 #define RED 6
-#define delayTime 10
-
-
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(RED, OUTPUT);
   pinMode(GREEN, OUTPUT);
   pinMode(BLUE, OUTPUT);
@@ -20,41 +17,14 @@ void setup() {
   srand(time(NULL));
 }
 
-//void loop() {
-//  for (int i = 4; i < 7; i++ ) {
-//    for (int x = 0; x < 200; x++) {
-//      analogWrite(i, x);
-//      delay(10);
-//    }
-//    for (int x = 200; x >=0; x--) {
-//          analogWrite(i, x);
-//          delay(10);
-//    }
-//  }
-//}
-
-//void loop() {
-//  for (int i = 4; i < 7; i++ ) {
-//    for (int x = 0; x < 75; x++) {
-//      analogWrite(i, x);
-//      delay(delayTime);
-//    }
-//    for (int x = 74; x >= 0; x--) {
-//        analogWrite(i, x);
-//        delay(delayTime);
-//
-//    }
-//  }
-//}
-int last, prev, prev_color, red_b, blue_b, green_b;
+int red_b, blue_b, green_b;
 
 void loop() {
   int random_color = (rand() % 3) + 4;
   int random_time = (rand() % 25) + 25;
   int random_brightness = (rand() % 50);
 
-  // if red
-  if (random_color == 4) {
+  if (random_color == RED) {
     if (random_brightness > red_b) {
       for (int i = red_b; i <= random_brightness; i++) {
         analogWrite(random_color, i);
@@ -70,8 +40,7 @@ void loop() {
     }
   }
 
-  // if green
-  if (random_color == 5) {
+  if (random_color == GREEN) {
     if (random_brightness > green_b) {
       for (int i = green_b; i <= random_brightness; i++) {
         analogWrite(random_color, i);
@@ -87,8 +56,7 @@ void loop() {
     }
   }
 
-  // if blue
-  if (random_color == 6) {
+  if (random_color == BLUE) {
     if (random_brightness > blue_b) {
       for (int i = blue_b; i <= random_brightness; i++) {
         analogWrite(random_color, i);
@@ -104,5 +72,4 @@ void loop() {
       blue_b = 0;
     }
   }
-  
 }
